3d4d2f53cb427c01700fdea416e686894a6e1d67
Nilansh Bansal <bansalnilansh@gmail.com>
2020-04-30 23:36:49 +0530

    final commit

--- emp/serializers.py
+++ emp/serializers.py
@@ -1,15 +1,29 @@
 from rest_framework import serializers
 from .models import Executive, Manager, ExecLeaveRequest, LeaveBalance
+from django.contrib.auth.hashers import make_password
 
 class ExecutiveSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
+    
     class Meta:
         model = Executive
-        fields = '__all__'
+        fields = ('url','Exec_No','First_Name','Last_Name','password','Birth_Date','Address','Mobile_Number','Email_Address','Hire_Date','Man_ID')
 
 class ManagerSerializer(serializers.HyperlinkedModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        help_text='Leave empty if no change needed',
+        style={'input_type': 'password', 'placeholder': 'Password'}
+    )
     class Meta:
         model = Manager
-        fields = '__all__'
+        fields = ('url','Man_No','First_Name','Last_Name','password','Birth_Date','Address','Mobile_Number','Email_Address','Hire_Date')
 
 class ExecLeaveRequestSerializer(serializers.ModelSerializer):
     class Meta:
